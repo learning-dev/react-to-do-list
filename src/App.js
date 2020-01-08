b7af8b5863e479410719f4b71f324f3e9f71bf13
learning-dev <ayazm.140895@gmail.com>
2020-01-08 23:44:43 +0530

    add header to todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
 
 import './App.css';
 
@@ -41,11 +42,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
   
 }
+
+
 
 export default App;
