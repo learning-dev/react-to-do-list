96d1a4c3e8d572c74795eededd411950ee87688b
learning-dev <ayazm.140895@gmail.com>
2020-01-08 23:02:50 +0530

    add functionality for checking items in todo-list when complete

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Complete the Todo list',
-        completed: false, 
+        completed: true, 
       }, 
       {
         id: 3,
@@ -24,10 +24,19 @@ class App extends Component {
     ]
   }
 
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) })
+  }
+
   render() {
     return (
       <div className="App">
-       <Todos todos={this.state.todos}/>
+       <Todos todos={this.state.todos} markComplete={this.markComplete}/>
       </div>
     );
   }
