8a599e4b2e3210fc4f8737d44c1537f2fe263a36
learning-dev <ayazm.140895@gmail.com>
2020-01-09 19:59:23 +0530

    add style to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,17 @@ import React from 'react'
 function About() {
   return (
     <React.Fragment>
-      <h1>About</h1>
-      <p>Todo list using React.</p>
+      <div style={aboutStyle}>
+        <h1 style={{padding: '10px'}}>About</h1>
+        <p><b>Todo list using React.</b></p>
+      </div>
     </React.Fragment>
   )
+}
+
+const aboutStyle = {
+  padding: '10px',
+  margin: '10px'
 }
 
 export default About;
